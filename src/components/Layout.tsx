990c55c4753e7e93c4d7c175f1d9edafcde22ce1
kimdowan <kimdowan1004@naver.com>
2025-08-22 20:31:22 +0900

    fix : min height 101vh

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,29 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import styled from "styled-components";
 
 export const Layout = () => {
   return (
-    <>
+    <PageLayout>
       <Header />
-      <Outlet />
+      <MainArea>
+        <Outlet />
+      </MainArea>
       <Footer />
-    </>
+    </PageLayout>
   );
 };
+
+const PageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 101vh;
+`;
+
+const MainArea = styled.main`
+  flex: 1 0 auto;
+  width: 100%;
+`;
 
 export default Layout;
